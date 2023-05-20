d545a0f99077881f65133ecf848d2d09c433de78
Raphael Busquet <raphaelbusquet@outlook.com>
2023-05-20 12:33:39 +0100

    stack navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import { SafeAreaView, Text } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+// Components
+import TextoCentral from './components/TextoCentral'
+import TelaA from './views/TelaA'
+import TelaB from './views/TelaB'
+import TelaC from './views/TelaC'
+
+const Stack = createNativeStackNavigator();
 
 export default props => (
-    <SafeAreaView>
-        <Text>App</Text>
-    </SafeAreaView>
+    <NavigationContainer>
+        <SafeAreaView style={{
+            flex: 1
+        }}>
+            <Stack.Navigator initialRouteName='TelaA'>
+                <Stack.Screen name='TelaA' component={TelaA} />
+                <Stack.Screen name='TelaB' component={TelaB} />
+                <Stack.Screen name='TelaC' component={TelaC} />
+            </Stack.Navigator>
+        </SafeAreaView>
+    </NavigationContainer>
 )
